0d6efc61ee7d7df04f08ac694817bce45be8ba1b
AleXitOTomaTitO <113984222+AleXitOTomaTitO@users.noreply.github.com>
2023-10-10 11:56:02 +0200

    cambiobase.php

---
+++ UT1/cambio_base/cambiobase.php
@@ -0,0 +1,86 @@
+<HTML>
+<HEAD><TITLE>Resultado</TITLE>
+<style>
+	h1{
+		margin-top:30px;
+		font-size:30px;
+		text-align:center;
+		margin-bottom:100px;
+	}
+	p{
+		text-align:center;
+	}
+	table,td{
+	border: 2px solid black;
+	border-collapse:collapse;
+	text-align:center;
+	}
+	table,th{
+		border: 2px solid black;
+		width:300px;
+		text-align:center;
+	}
+</style>
+</HEAD>
+<BODY>
+<h1>CONVERSOR NUMERICO</h1>
+<?php
+$num=limpio($_POST["dec"]);;
+$opera=$_POST["ope"];
+
+function ope($n,$dato){
+	$resul=0;
+	$msg="";
+	
+	switch($dato){
+		case "bina":
+		$resul=decbin($n);
+		$msg="<p>$n en Binario es $resul</p>";
+		break;
+		
+		case "octa":
+		$resul=decoct($n);
+		$msg="<p>$n en Octal es $resul</p>";
+		break;
+		
+		case "hexa":
+		$resul=strtoupper(dechex($n));
+		$msg="<p>$n en Hexadecimal es $resul</p>";
+		break;
+		
+		case "todo":
+			echo "<table>";
+			echo "<tr>";
+			echo "<td>Binario</td>";
+			$resul=decbin($n);
+			echo "<td>$resul</td>";
+			
+			echo "<tr>";
+			echo "<td>Ocatal</td>";
+			$resul=decoct($n);
+			echo "<td>$resul</td>";
+			
+			echo "<tr>";
+			echo "<td>Hexadecimal</td>";
+			$resul=strtoupper(dechex($n));;
+			echo "<td>$resul</td>";
+			
+			echo "</table>";
+			break;
+		
+	}
+	return $msg;
+}
+
+function limpio($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+echo ope($num,$opera);	
+?>
+</BODY>
+</HTML>
