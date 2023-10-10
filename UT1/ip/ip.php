e0e9756e336e36614f2100441a805f3cffa5c3fb
AleXitOTomaTitO <113984222+AleXitOTomaTitO@users.noreply.github.com>
2023-10-10 11:59:17 +0200

    ip.php

---
+++ UT1/ip/ip.php
@@ -0,0 +1,90 @@
+
+<?php
+
+$res="";
+	$err="";
+	$num="";
+	
+		if(empty($_POST["ip"])){
+			$err = "IP Requerido";
+			$res="";
+		}
+		else{
+			$n=limpia($_POST["ip"]);
+			$res=conver($n);
+		}
+	
+	
+	function conver ($ip){
+		$res="";
+		$comp=true;
+		//PASAR TODO A BINARIO
+	$p1 =strpos($ip,".",0); // busca en la ip, un punto, desde la posicion 0
+	$p2 =strpos($ip,".",($p1+1));
+	$p3 =strpos($ip,".",($p2+1));
+	//PRIMER OCTETO
+	$oct1= substr($ip,0,$p1);
+	if($oct1>=1 && $oct1<=255){
+		
+	}else{
+		$comp=false;
+		$res="El primer octeto es incorrecto";
+	}
+
+	//SEGUNDO OCTETO
+	$oct2= substr($ip,($p1+1),($p2-$p1));
+	if($oct2>=1 && $oct2<=255){
+		
+	}else{
+		$comp=false;
+		$res="El segundo octeto es incorrecto ,$res";
+	}
+
+	//TERCER OCTETO
+	$oct3= substr($ip,($p2+1),($p3-$p2));
+	if($oct3>=1 && $oct3<=255){
+		
+	}else{
+		$comp=false;
+		$res="El tercer octeto es incorrecto ,$res";
+	}
+
+	//CUARTO OCTETO
+	$oct4= substr($ip,($p3+1),$p3);
+	if($oct4>=1 && $oct4<=255){
+		
+	}else{
+		$comp=false;
+		$res="El cuarto octeto es incorrecto ,$res";
+	}
+	
+		if($comp==true){
+			$res=(str_pad(decbin($oct1),8,"0",STR_PAD_LEFT).".".
+			str_pad(decbin($oct2),8,"0",STR_PAD_LEFT).".".
+			str_pad(decbin($oct3),8,"0",STR_PAD_LEFT).".".
+			str_pad(decbin($oct4),8,"0",STR_PAD_LEFT));
+		}
+		return $res;
+	}
+	
+	function comprobar($num){
+		if($num>=1 && $num<=255){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	
+
+	function limpia($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+
+		echo "<br><h3>RESULTADO</h3>";
+		echo $res;
+
+?>
